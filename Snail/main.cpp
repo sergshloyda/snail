4304e999b344469f76dbe9bbc2ba48161939fcb2
sergshloyda <shloyders@gmail.com>
2022-04-13 18:09:24 +0300

    test static

--- Snail/main.cpp
+++ Snail/main.cpp
@@ -1,7 +1,8 @@
 #include "snail.h"
 #include <QtWidgets/QApplication>
 #include <qlabel.h>
-
+#include <QtPlugin>
+//Q_IMPORT_PLUGIN (QWindowsIntegrationPlugin)
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
